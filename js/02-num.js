7d7037ad8d8e472fde33d533673a8bd76aa1b791
arnyss <114816462+arnyss@users.noreply.github.com>
2022-10-14 13:43:46 +0300

    update

---
+++ js/02-num.js
@@ -0,0 +1,10 @@
+const imgSrc = "./img/logo.png";
+const HTML = `<header>
+      <img src="${imgSrc}" alt="LOGO" class="logo" />
+      <nav>\n\
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+      </nav>
+    </header>`;
+console.log(HTML);
